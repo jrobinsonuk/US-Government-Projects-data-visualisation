affec60254a395c213a41a88556504977816dacd
James Robinson <jamesrobinson.uk@me.com>
2014-03-08 19:53:29 +0000

    Bubbles visualisation supports data in and transitioning.

--- js/visualisation-bubbles.js
+++ js/visualisation-bubbles.js
@@ -1,10 +1,24 @@
+var selectedAgency = null;
+
+var svgSize = {width:1000, height:800};
+
+var svg = d3.select("#bubbles").append("svg")
+							.attr("width", svgSize.width)
+							.attr("height", svgSize.height);
+
+
+var formatNumber = d3.format(",d");
+
+
+
+
+
 d3.csv("data/projects-1.0.csv", function(error, projects) {
 	// Projects loaded. We have an array as 'projects'
 
 
 	// Gather information about agencies
 	var agencies = new Array();
-	agencies.push({"name":"test", "code":100, "projectCount":1});
 	projects.forEach(function(p, i) {
 		var foundAgency = agencies.filter(function (agency) {
 			return (agency.code == p.agencyCode);
@@ -45,20 +59,132 @@ d3.csv("data/projects-1.0.csv", function(error, projects) {
 		return 0;
 	});
 
-	console.log("agencies = "+ JSON.stringify(agencies));
+
+	// Get drawing
+	drawVisualisaton();
+
+
+	d3.select("body").select("#total").text(formatNumber(projects.length));
+	d3.select("body").select("#back").on("click", function () {
+		selectedAgency = null;
+		drawVisualisaton();
+	});
 
 
 
 
-	// Initial - display agencies with number of active projects
-	// size of bubble depends on projectCount
-	// colour of bubble depends on costMoreCount percentage (greater => deeper green), costLessCount percentage (greater => deeper red)
-	// summary stats for all agencies are put into progress bars
+	function drawVisualisaton() {
+		// Clear
+		// svg.text('');
+		d3.selectAll('.d3-tip').remove();
+		svg.selectAll('circle').remove();
+
+		// Prepare to add data
+		var force = d3.layout.force()
+				        .size([svgSize.width, svgSize.height])
+				        .linkDistance(10)
+				        .on("tick", tick);
+
+		var tip = d3.tip()
+						.attr("class", "d3-tip")
+						.offset([-10, 0]);
+		svg.call(tip);
+		
+
+		if (selectedAgency == null) {
+			// Initial - display agencies with number of active projects
+			// size of bubble depends on projectCount
+			// colour of bubble depends on costMoreCount percentage (greater => deeper green), costLessCount percentage (greater => deeper red)
+			// summary stats for all agencies are put into progress bars
+
+
+			// Update existing stuff
+			force.nodes(agencies)
+					.charge(function(d, i) {return d.projectCount/5.0 * (-10); })
+					.start();
+
+			tip.html(function (d) {
+				return '<strong>' + d.name + '</strong>';
+			});
+
+
+			// Create nodes
+			var node = svg.selectAll(".node")
+							.data(force.nodes())
+					        .attr("class", "node")
+					        .enter().append("g")
+					        .call(force.drag)
+					        .on("click", agencyClick)
+					        .on('mouseover', tip.show)
+							.on('mouseout', tip.hide);
+
+			node.append("circle")
+					.attr("r", function (d, i) { return d.projectCount/5; })
+					.style("fill", "#ff00ff")
+					.style("stroke", "#000000");
+
+
+			d3.select("body").select("#active").text(formatNumber(projects.length));
+			d3.select("body").select("#totals").style("display", "none");
+			d3.select("body").select("#back").style("display", "none");
+
+		} else {
+			// Projects - filtered from agency code
+			// Size dependant on actual cost
+			// colour of bubble depends on variance (negative => green, less => deeper), variance (positive => red, more => deeper)
+			// summary stats for selected agency are put into progress bars
+
+			var agencyProjects = projects.filter(function (project) {
+				return (project.agencyCode == selectedAgency.code);
+			});
+
+
+			// Update existing stuff
+			force.nodes(agencyProjects)
+					.charge(function(d, i) {return 15 * (-10); })
+					.start();
+
+			tip.html(function (d) {
+				return '<strong>' + d.projectName + '</strong>';
+			});
+
+
+			// Create nodes
+			var node = svg.selectAll(".node")
+							.data(force.nodes())
+					        .attr("class", "node")
+					        .enter().append("g")
+					        .call(force.drag)
+					        .on('mouseover', tip.show)
+							.on('mouseout', tip.hide);
+
+			node.append("circle")
+					.attr("r", function (d, i) { return 15; })
+					.style("fill", "#ff0000")
+					.style("stroke", "#000000");
+
+
+			d3.select("body").select("#active").text(formatNumber(agencyProjects.length));
+			d3.select("body").select("#totals").style("display", "");
+			d3.select("body").select("#back").style("display", "");
+		}
 
 
 
-	// Projects - filtered from agency code
-	// Size dependant on actual cost
-	// colour of bubble depends on variance (negative => green, less => deeper), variance (positive => red, more => deeper)
-	// summary stats for selected agency are put into progress bars
+
+		function tick() {
+	        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	    }
+
+	    function agencyClick(d) {
+	    	if (d3.event.defaultPrevented) return; // click suppressed
+			tip.hide;
+			selectedAgency = d;
+			drawVisualisaton();
+	    }
+
+	}
+
+
+	
 });
