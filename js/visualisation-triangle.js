ffd04784d665db648fd04dc0af4e26ceff9e417d
James Robinson <jamesrobinson.uk@me.com>
2014-03-10 13:42:22 +0000

    Triangles are drawn.  Not sure about how well they are representing data though

--- js/visualisation-triangle.js
+++ js/visualisation-triangle.js
@@ -29,23 +29,80 @@ d3.csv("data/projects-1.0.csv", function(error, projects) {
 
 
 	function drawTriangle() {
-		var _leftTriangle = [{x:200, y:5}, {x:200, y:255}, {x:40, y:255}, {x:200, y:5}],
-			_rightTriangle = [{x:200, y:5}, {x:200, y:255}, {x:360, y:255}, {x:200, y:5}];
+		var planPercent = 0.8,
+			under = true;
+
+		var _leftTriangle = [{x:160, y:0}, {x:160, y:250}, {x:0, y:250}, {x:160, y:0}],
+			_leftRule = [{x:(160*planPercent), y:(250*(1-planPercent))}, {x:160, y:(250*(1-planPercent))}],
+			_rightTriangle = [{x:160, y:0}, {x:160, y:250}, {x:320, y:250}, {x:160, y:0}],
+			_rightRule = [{x:160 + (160*(1-planPercent)), y:(250*(1-planPercent))}, {x:160, y:(250*(1-planPercent))}]
+			;
 
 
 		var node = svg.append("g")
 
+		// Left half
 		node.append("path")
 			.attr("d", lineDrawer(_leftTriangle))
 			.attr("stroke", "blue")
-			.attr("stroke-width", 1)
+			.attr("stroke-width", 0.5)
 			.attr("fill", "none");
 
+		if (planPercent < 1) {
+			// Fill
+			var _actualFill = [{x:(160*planPercent), y:(250*(1-planPercent))}, {x:160, y:(250*(1-planPercent))}];
+			if (under) {
+				_actualFill.push({x:160, y:(250*2*(1-planPercent))});
+				_actualFill.push({x:160*(planPercent-(1-planPercent)), y:(250*2*(1-planPercent))});
+			} else {
+				_actualFill.push({x:160, y:0});
+			}
+			_actualFill.push(_actualFill[0]); // finish off
+
+			node.append("path")
+				.attr("d", lineDrawer(_actualFill))
+				.attr("fill", (under)? "green" : "red");
+
+			// Budget line
+			node.append("path")
+				.attr("d", lineDrawer(_leftRule))
+				.attr("stroke", "black")
+				.attr("stroke-width", 2);
+		}
+
+
+
+		under = false;
+
+		// Right half
 		node.append("path")
 				.attr("d", lineDrawer(_rightTriangle))
 				.attr("stroke", "blue")
-				.attr("stroke-width", 1)
+				.attr("stroke-width", 0.5)
 				.attr("fill", "none");
+
+		if (planPercent < 1) {
+			// Fill
+			var _actualFill = [{x:160 + (160*(1-planPercent)), y:(250*(1-planPercent))}, {x:160, y:(250*(1-planPercent))}];
+			if (under) {
+				_actualFill.push({x:160, y:(250*2*(1-planPercent))});
+				_actualFill.push({x:160+160*(1-(planPercent-(1-planPercent))), y:(250*2*(1-planPercent))});
+			} else {
+				_actualFill.push({x:160, y:0});
+			}
+			_actualFill.push(_actualFill[0]); // finish off
+
+			node.append("path")
+				.attr("d", lineDrawer(_actualFill))
+				.attr("fill", (under)? "green" : "red");
+
+
+			// Budget line
+			node.append("path")
+				.attr("d", lineDrawer(_rightRule))
+				.attr("stroke", "black")
+				.attr("stroke-width", 2);
+		}
 
 
 		return node;
